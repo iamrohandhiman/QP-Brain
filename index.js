ce5c29f0c5eacc72b54c1e5b541bc28c809d5d43
iamrohandhiman <iamrohandhiman@gmail.com>
2024-04-24 20:18:28 +0530

    added dynamic dictionary based distribution for theory Part

--- index.js
+++ index.js
@@ -38,82 +38,149 @@ function generateQuestionPaper(module, marks, count, type, flag) {
   }
 }
 
+
 // Arrays to hold the generated question papers
 var qp1 = [];
 var qp2 = [];
 var qp3 = [];
 
-let dictAT = {
- "1": "2",
- "2": "1",
- "3": "3"
+
+
+const chapters = [1, 2, 3];
+const weights = [6, 9, 10];
+const TnRatio = 80;
+const chpNumericalRatio = [40,50,60]
+
+
+let nqAT = Math.round(TnRatio * 0.01 * 7);
+let nqBT = Math.round(TnRatio * 0.01 * 4);
+
+let nqAN = 7 - nqAT;
+let nqBN = 7 - nqBT;
+
+// Finding maximum weight
+let maxiIndex = 0;
+for (let i = 1; i < weights.length; i++) { 
+    if (weights[i] > weights[maxiIndex]) {
+        maxiIndex = i;
+    }
 }
 
-let dictBT = {
-  "1": "1",
-  "2": "1",
-  "3": "1"
- }
+let chpMaxi = chapters[maxiIndex];
 
-let dictAN = {
-  "1": "0",
-  "2": "1",
-  "3": "0"
- }
+//populate sectionA theory
+let dictAT = {};
+chapters.forEach(chapter => dictAT[chapter] = 0);
+
+let indexAT = 0;
+let questionCountAT = 0;
+
+
+if (nqAT % 2 === 0) {
+    while (questionCountAT < nqAT) {
+        dictAT[chapters[indexAT]] += 1; 
+        indexAT = (indexAT + 1) % chapters.length; 
+        questionCountAT++;
+    }
+} else { 
+    while (questionCountAT < (nqAT - 1)) {
+        dictAT[chapters[indexAT]] += 1;
+        indexAT = (indexAT + 1) % chapters.length;
+        questionCountAT++;
+    }
+    dictAT[chpMaxi] += 1; 
+}
+
+console.log(dictAT);
+
+//populating questions(Theory) in section B
+let dictBT = {};
+chapters.forEach(chapter => dictBT[chapter] = 0); 
+
+
+let indexBT = 0;
+let questionCountBT = 0;
+
+
+if (nqBT % 2 === 0) {
+    while (questionCountBT < nqBT) { 
+        dictBT[chapters[indexBT]] += 1; 
+        indexBT = (indexBT + 1) % chapters.length; 
+        questionCountBT++; 
+    }
+} else { 
+    while (questionCountBT < (nqBT - 1)) { 
+        dictBT[chapters[indexBT]] += 1;
+        indexBT = (indexBT + 1) % chapters.length;
+        questionCountBT++;
+    }
+    dictBT[chpMaxi] += 1; 
+}
+
+console.log(dictBT); 
+
+
+
+
+// let dictAN = {
+//   "1": "0",
+//   "2": "1",
+//   "3": "0"
+//  }
  
-let dictBN = {
-   "1": "0",
-   "2": "1",
-   "3": "0"
-  }
+// let dictBN = {
+//    "1": "0",
+//    "2": "1",
+//    "3": "0"
+//   }
+
+// // Generate question papers and store them in arrays
+// for (let i = 1; i <= 3; i++) {
+//   const { set1, set2, set3 } = generateQuestionPaper(i, 2, parseInt(dictAT[i]), "T", true); //module marks count 
+//   qp1.push(set1);
+  
+//   qp2.push(set2);
+  
+//   qp3.push(set3);
+  
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//   const { set1, set2, set3 } = generateQuestionPaper(i, 2, parseInt(dictAN[i]), "N", true); //module marks count 
+//   qp1.push(set1);
+  
+//   qp2.push(set2);
+  
+//   qp3.push(set3);
+  
+// }
 
 // Generate question papers and store them in arrays
-for (let i = 1; i <= 3; i++) {
-  const { set1, set2, set3 } = generateQuestionPaper(i, 2, parseInt(dictAT[i]), "T", true); //module marks count 
-  qp1.push(set1);
+// for (let i = 1; i <= 3; i++) {
+//   const { set1, set2, set3 } = generateQuestionPaper(i, 5, parseInt(dictBT[i]), "T", true); //module marks count 
+//   qp1.push(set1);
   
-  qp2.push(set2);
+//   qp2.push(set2);
   
-  qp3.push(set3);
+//   qp3.push(set3);
   
-}
+// }
 
-for (let i = 1; i <= 3; i++) {
-  const { set1, set2, set3 } = generateQuestionPaper(i, 2, parseInt(dictAN[i]), "N", true); //module marks count 
-  qp1.push(set1);
+// // Generate question papers and store them in arrays
+// for (let i = 1; i <= 3; i++) {
+//   const { set1, set2, set3 } = generateQuestionPaper(i, 5, parseInt(dictBN[i]), "N", true); //module marks count 
+//   qp1.push(set1);
   
-  qp2.push(set2);
+//   qp2.push(set2);
   
-  qp3.push(set3);
+//   qp3.push(set3);
   
-}
-
-// Generate question papers and store them in arrays
-for (let i = 1; i <= 3; i++) {
-  const { set1, set2, set3 } = generateQuestionPaper(i, 5, parseInt(dictBT[i]), "N", true); //module marks count 
-  qp1.push(set1);
-  
-  qp2.push(set2);
-  
-  qp3.push(set3);
-  
-}
-
-// Generate question papers and store them in arrays
-for (let i = 1; i <= 3; i++) {
-  const { set1, set2, set3 } = generateQuestionPaper(i, 5, parseInt(dictBN[i]), "N", true); //module marks count 
-  qp1.push(set1);
-  
-  qp2.push(set2);
-  
-  qp3.push(set3);
-  
-}
+// }
 
 
-console.log("Set 1:");
-console.log(qp1);
-console.log("Set 2:");
-console.log(qp2);
-console.log("Set 3:");
-console.log(qp3);
+// console.log("Set 1:");
+// console.log(qp1);
+// console.log("Set 2:");
+// console.log(qp2);
+// console.log("Set 3:");
+// console.log(qp3);
